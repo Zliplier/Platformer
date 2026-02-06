1dff84b8c2ca74c17f7609ad3a141d175d378982
Zliplier <ktiranakul@gmail.com>
2026-02-06 15:09:06 +0700

    Start Combat

--- Assets/Player/Data/Combat/AttackFrame.cs
+++ Assets/Player/Data/Combat/AttackFrame.cs
@@ -1,10 +1,23 @@
 ﻿using System;
+using UnityEngine;
 
 namespace Player.Data.Combat
 {
     [Serializable]
     public class AttackFrame
     {
-        
+        public float startTime;
+        public float endTime;
+
+        public HItBox[] hitBoxes;
+    
+        public bool allowMovement = false;
+    }
+    
+    [Serializable]
+    public struct HItBox
+    {
+        public Vector3 hitboxOffset;
+        public Vector3 hitboxSize;
     }
 }
